35b901e5fbecc2556263383dd1a86c05395769b0
qiuxiangdong <qiuxiangdong3@huawei.com>
2022-03-25 10:14:10 +0800

    fix: Adjusting Log Level
    Signed-off-by: qiuxiangdong <qiuxiangdong3@huawei.com>

--- services/native/src/shutdown_service.cpp
+++ services/native/src/shutdown_service.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -83,7 +83,7 @@ void ShutdownService::DelShutdownCallback(const sptr<IShutdownCallback>& callbac
 
 bool ShutdownService::IsShuttingDown()
 {
-    POWER_HILOGE(MODULE_SERVICE, "IsShuttingDown");
+    POWER_HILOGD(MODULE_SERVICE, "IsShuttingDown");
     return started_;
 }
 
