17de508c15b0d01eb4900ee228c12381c76ba3b9
qiuxiangdong <qiuxiangdong3@huawei.com>
2022-04-25 20:06:11 +0800

    feat: add power mode interface
    Signed-off-by: qiuxiangdong <qiuxiangdong3@huawei.com>

--- frameworks/napi/power/power.cpp
+++ frameworks/napi/power/power.cpp
@@ -25,6 +25,10 @@ using namespace OHOS::PowerMgr;
 namespace {
 constexpr int REASON_MAX = 512;
 constexpr int RESULT_SIZE = 2;
+constexpr int ARGC_ONE = 1;
+constexpr int ARGC_TWO = 2;
+constexpr int INDEX_ONE = 0;
+constexpr int INDEX_TWO = 1;
 }
 
 struct PowerAsyncCallbackInfo {
@@ -33,6 +37,7 @@ struct PowerAsyncCallbackInfo {
     napi_ref callbackRef = nullptr;
     napi_deferred deferred = nullptr;
     bool screenOn = false;
+    uint32_t powerMode = PowerMgrClient::NORMAL_MODE;
 };
 
 static PowerMgrClient &g_powerMgrClient = PowerMgrClient::GetInstance();
@@ -188,6 +193,192 @@ static napi_value SuspendDevice(napi_env env, napi_callback_info info)
     return nullptr;
 }
 
+static void GetPowerModeCallBack(napi_env env, PowerAsyncCallbackInfo *asyncCallbackInfo)
+{
+    napi_value resource = nullptr;
+    napi_create_string_utf8(env, "GetPowerMode", NAPI_AUTO_LENGTH, &resource);
+    napi_create_async_work(
+        env,
+        nullptr,
+        resource,
+        [](napi_env env, void *data) {
+            PowerAsyncCallbackInfo *asyncCallbackInfo = (PowerAsyncCallbackInfo *)data;
+            asyncCallbackInfo->powerMode = g_powerMgrClient.GetDeviceMode();
+            POWER_HILOGD(COMP_FWK, "powerMode is %{public}u ", asyncCallbackInfo->powerMode);
+        },
+        [](napi_env env, napi_status status, void *data) {
+            PowerAsyncCallbackInfo *asyncCallbackInfo = (PowerAsyncCallbackInfo *)data;
+            napi_value result[ARGC_TWO] = { 0 };
+            napi_create_uint32(env, asyncCallbackInfo->powerMode, &result[INDEX_TWO]);
+            if (asyncCallbackInfo->deferred) {
+                napi_resolve_deferred(env, asyncCallbackInfo->deferred, result[INDEX_TWO]);
+            } else {
+                napi_value tmp = nullptr;
+                napi_value callback = nullptr;
+                napi_get_reference_value(env, asyncCallbackInfo->callbackRef, &callback);
+                napi_get_undefined(env, &result[INDEX_ONE]);
+                napi_call_function(env, nullptr, callback, ARGC_TWO, result, &tmp);
+                napi_delete_reference(env, asyncCallbackInfo->callbackRef);
+            }
+            napi_delete_async_work(env, asyncCallbackInfo->asyncWork);
+            delete asyncCallbackInfo;
+        },
+        (void *)asyncCallbackInfo,
+        &asyncCallbackInfo->asyncWork);
+    napi_queue_async_work(env, asyncCallbackInfo->asyncWork);
+}
+
+static napi_value GetPowerMode(napi_env env, napi_callback_info info)
+{
+    size_t argc = ARGC_ONE;
+    napi_value args[ARGC_ONE] = { 0 };
+    napi_value jsthis;
+    void *data = nullptr;
+
+    napi_status status = napi_get_cb_info(env, info, &argc, args, &jsthis, &data);
+    NAPI_ASSERT(env, (status == napi_ok), "Failed to get cb info");
+
+    auto asyncCallbackInfo = new PowerAsyncCallbackInfo();
+    if (asyncCallbackInfo == nullptr) {
+        POWER_HILOGE(COMP_FWK, "Failed to create asyncCallbackInfo");
+        return nullptr;
+    }
+
+    napi_valuetype type;
+    if (argc == ARGC_ONE) {
+        NAPI_CALL(env, napi_typeof(env, args[INDEX_ONE], &type));
+        if (type != napi_function) {
+            POWER_HILOGE(COMP_FWK, "Wrong argument type. napi_function expected");
+            delete asyncCallbackInfo;
+            return nullptr;
+        }
+        napi_create_reference(env, args[INDEX_ONE], 1, &asyncCallbackInfo->callbackRef);
+    }
+    napi_value result = nullptr;
+    if (asyncCallbackInfo->callbackRef == nullptr) {
+        POWER_HILOGD(COMP_FWK, "callbackRef is null");
+        napi_create_promise(env, &asyncCallbackInfo->deferred, &result);
+    } else {
+        POWER_HILOGD(COMP_FWK, "callbackRef is not null");
+        napi_get_undefined(env, &result);
+    }
+    GetPowerModeCallBack(env, asyncCallbackInfo);
+    return result;
+}
+
+static void SetPowerModeCallBack(napi_env env, PowerAsyncCallbackInfo *asyncCallbackInfo)
+{
+    napi_value resource = nullptr;
+    napi_create_string_utf8(env, "SetDeviceMode", NAPI_AUTO_LENGTH, &resource);
+    napi_create_async_work(
+        env,
+        nullptr,
+        resource,
+        [](napi_env env, void *data) {
+            PowerAsyncCallbackInfo *asyncCallbackInfo = (PowerAsyncCallbackInfo *)data;
+            g_powerMgrClient.SetDeviceMode(asyncCallbackInfo->powerMode);
+        },
+        [](napi_env env, napi_status status, void *data) {
+            PowerAsyncCallbackInfo *asyncCallbackInfo = (PowerAsyncCallbackInfo *)data;
+            if (asyncCallbackInfo->deferred) {
+                napi_resolve_deferred(env, asyncCallbackInfo->deferred, nullptr);
+            } else {
+                napi_value tmp = nullptr;
+                napi_value callback = nullptr;
+                napi_get_reference_value(env, asyncCallbackInfo->callbackRef, &callback);
+                napi_call_function(env, nullptr, callback, 0, nullptr, &tmp);
+                napi_delete_reference(env, asyncCallbackInfo->callbackRef);
+            }
+            napi_delete_async_work(env, asyncCallbackInfo->asyncWork);
+            delete asyncCallbackInfo;
+        },
+        (void *)asyncCallbackInfo,
+        &asyncCallbackInfo->asyncWork);
+    napi_queue_async_work(env, asyncCallbackInfo->asyncWork);
+}
+
+static napi_value SetPowerMode(napi_env env, napi_callback_info info)
+{
+    size_t argc = ARGC_TWO;
+    napi_value args[ARGC_TWO] = { 0 };
+    napi_value jsthis;
+    void *data = nullptr;
+
+    napi_status status = napi_get_cb_info(env, info, &argc, args, &jsthis, &data);
+    NAPI_ASSERT(env, (status == napi_ok), "Failed to get cb info");
+
+    auto asyncCallbackInfo = new PowerAsyncCallbackInfo();
+    if (asyncCallbackInfo == nullptr) {
+        POWER_HILOGE(COMP_FWK, "Failed to create asyncCallbackInfo");
+        return nullptr;
+    }
+    napi_valuetype type;
+    if (argc == ARGC_TWO) {
+        napi_value callback = args[INDEX_TWO];
+        NAPI_CALL(env, napi_typeof(env, callback, &type));
+        if (type != napi_function) {
+            POWER_HILOGE(COMP_FWK, "Wrong argument type. napi_function expected");
+            delete asyncCallbackInfo;
+            return nullptr;
+        }
+        napi_create_reference(env, callback, 1, &asyncCallbackInfo->callbackRef);
+    }
+
+    napi_value mode = args[INDEX_ONE];
+    napi_get_value_uint32(env, mode, &asyncCallbackInfo->powerMode);
+
+    napi_value result = nullptr;
+    if (asyncCallbackInfo->callbackRef == nullptr) {
+        POWER_HILOGD(COMP_FWK, "callbackRef is null");
+        napi_create_promise(env, &asyncCallbackInfo->deferred, &result);
+    } else {
+        POWER_HILOGD(COMP_FWK, "callbackRef is not null");
+        napi_get_undefined(env, &result);
+    }
+    SetPowerModeCallBack(env, asyncCallbackInfo);
+    return result;
+}
+
+static napi_value EnumPowerModeClassConstructor(napi_env env, napi_callback_info info)
+{
+    napi_value thisArg = nullptr;
+    void* data = nullptr;
+
+    napi_get_cb_info(env, info, nullptr, nullptr, &thisArg, &data);
+
+    napi_value global = nullptr;
+    napi_get_global(env, &global);
+
+    return thisArg;
+}
+
+static napi_value CreateDevicePowerMode(napi_env env, napi_value exports)
+{
+    napi_value performance = nullptr;
+    napi_value normal = nullptr;
+    napi_value powerSave = nullptr;
+    napi_value extremePowerSave = nullptr;
+
+    napi_create_int32(env, (int32_t)PowerMgrClient::PERFORMANCE, &performance);
+    napi_create_int32(env, (int32_t)PowerMgrClient::NORMAL_MODE, &normal);
+    napi_create_int32(env, (int32_t)PowerMgrClient::POWER_SAVE_MODE, &powerSave);
+    napi_create_int32(env, (int32_t)PowerMgrClient::EXTREME_MODE, &extremePowerSave);
+
+    napi_property_descriptor desc[] = {
+        DECLARE_NAPI_STATIC_PROPERTY("MODE_PERFORMANCE", performance),
+        DECLARE_NAPI_STATIC_PROPERTY("MODE_NORMAL", normal),
+        DECLARE_NAPI_STATIC_PROPERTY("MODE_POWER_SAVE", powerSave),
+        DECLARE_NAPI_STATIC_PROPERTY("MODE_EXTREME_POWER_SAVE", extremePowerSave),
+    };
+    napi_value result = nullptr;
+    napi_define_class(env, "DevicePowerMode", NAPI_AUTO_LENGTH, EnumPowerModeClassConstructor, nullptr,
+        sizeof(desc) / sizeof(*desc), desc, &result);
+
+    napi_set_named_property(env, exports, "DevicePowerMode", result);
+
+    return exports;
+}
+
 EXTERN_C_START
 /*
  * function for module exports
@@ -201,8 +392,11 @@ static napi_value PowerInit(napi_env env, napi_value exports)
         DECLARE_NAPI_FUNCTION("isScreenOn", IsScreenOn),
         DECLARE_NAPI_FUNCTION("wakeupDevice", WakeupDevice),
         DECLARE_NAPI_FUNCTION("suspendDevice", SuspendDevice),
+        DECLARE_NAPI_FUNCTION("getPowerMode", GetPowerMode),
+        DECLARE_NAPI_FUNCTION("setPowerMode", SetPowerMode)
     };
     NAPI_CALL(env, napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc));
+    CreateDevicePowerMode(env, exports);
     POWER_HILOGD(COMP_FWK, "The initialization of the Power module is complete");
 
     return exports;
