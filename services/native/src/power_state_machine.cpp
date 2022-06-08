93c6b207bb095f1701ae5a674ffcc8777e2eebc4
shijie <shijie20@huawei.com>
2022-06-08 10:30:27 +0800

    refactor: create powermanager.yaml file for power HiSysEvent
    Signed-off-by: shijie <shijie20@huawei.com>

--- services/native/src/power_state_machine.cpp
+++ services/native/src/power_state_machine.cpp
@@ -1046,14 +1046,8 @@ void PowerStateMachine::StateController::RecordFailure(PowerState from,
         .append("\n");
     const int logLevel = 2;
     const std::string tag = "TAG_POWER";
-    HiviewDFX::HiSysEvent::Write(HiviewDFX::HiSysEvent::Domain::POWERMGR, "POWER_SCREEN",
-        HiviewDFX::HiSysEvent::EventType::FAULT,
-        "LOG_LEVEL",
-        logLevel,
-        "TAG",
-        tag,
-        "MESSAGE",
-        message);
+    HiviewDFX::HiSysEvent::Write("POWER", "POWER_SCREEN",
+        HiviewDFX::HiSysEvent::EventType::FAULT, "LOG_LEVEL", logLevel, "TAG", tag, "MESSAGE", message);
     POWER_HILOGI(FEATURE_POWER_STATE, "RecordFailure: %{public}s", message.c_str());
 }
 } // namespace PowerMgr
