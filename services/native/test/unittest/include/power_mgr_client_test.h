169975f106b857e49094c34bf9dd00f3952012db
yangziyong <nsyangziyong@huawei.com>
2022-11-02 20:54:14 +0800

    test: 缩短测试时长
    Signed-off-by: yangziyong <nsyangziyong@huawei.com>

--- services/native/test/unittest/include/power_mgr_client_test.h
+++ services/native/test/unittest/include/power_mgr_client_test.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,8 +13,8 @@
  * limitations under the License.
  */
 
-#ifndef POWERMGR_POWERMGR_ST_MOCK_TEST_H
-#define POWERMGR_POWERMGR_ST_MOCK_TEST_H
+#ifndef POWERMGR_POWERMGR_CLIENT_TEST_H
+#define POWERMGR_POWERMGR_CLIENT_TEST_H
 
 #include <gtest/gtest.h>
 #include <ipc_skeleton.h>
@@ -23,33 +23,29 @@
 #include "mock_power_action.h"
 #include "mock_state_action.h"
 #include "power_common.h"
+#include "power_mgr_client.h"
 #include "power_mgr_service.h"
+#include "power_state_machine_info.h"
 
 namespace OHOS {
 namespace PowerMgr {
 constexpr int NEXT_WAIT_TIME_S = 1;
 constexpr int ASYNC_WAIT_TIME_S = 3;
 constexpr int REFRESHACTIVITY_WAIT_TIME_S = 8;
-constexpr int SCREEN_OFF_WAIT_TIME_S = 1;
-constexpr int SCREEN_DIM_WAIT_TIME_S = (SCREEN_OFF_WAIT_TIME_S / 2);
-constexpr int SLEEP_WAIT_TIME_S = (DEFAULT_SLEEP_TIME / 1000);
+int32_t SCREEN_OFF_WAIT_TIME_MS = 1000;
+int32_t SCREEN_OFF_WAIT_TIME_S = 1;
+int32_t SCREEN_DIM_WAIT_TIME_S = (SCREEN_OFF_WAIT_TIME_S / 2);
+constexpr int SLEEP_WAIT_TIME_S = 1;
 constexpr int SLEEP_WAIT_TIME_MS = 100;
-constexpr int SET_DISPLAY_OFF_TIME_MS = 8000;
+constexpr int SET_DISPLAY_OFF_TIME_MS = 1000;
+constexpr int TRANSFER_NS_TO_MS = 1000000;
 constexpr int TRANSFER_MS_TO_S = 1000;
-constexpr int TRANSFER_NS_TO_MS = 1000;
 constexpr float DIM_RATE = 0.8;
 constexpr int DOUBLE_TIMES = 2;
-constexpr int TEST_RATE = 3;
-constexpr int ONE_SECOND = 1;
 
-class PowerMgrSTMockTest : public testing::Test {
-public:
-    static void SetUpTestCase(void);
-    static void TearDownTestCase(void);
-    void SetUp();
-    void TearDown();
+class PowerMgrClientTest : public testing::Test {
 };
 } // namespace PowerMgr
 } // namespace OHOS
 
-#endif // POWERMGR_POWERMGR_MOCK_TEST_H
+#endif // POWERMGR_POWERMGR_CLIENT_TEST_H
