522f96df771e28f32b3a651d85dd5e8871c0a31d
yangzk <yangzhongkai@huawei.com>
2022-12-14 16:53:06 +0800

    fix: use new Ability interface
    Signed-off-by: yangzk <yangzhongkai@huawei.com>
    Change-Id: I2249e8978ab7dafc6b7128f23471b27147763f82

--- power_dialog/entry/src/main/ets/MainAbility/MainAbility.ts
+++ power_dialog/entry/src/main/ets/MainAbility/MainAbility.ts
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 
-import Ability from '@ohos.application.Ability';
+import Ability from '@ohos.app.ability.UIAbility';
 
 var TAG = "Dialog_MainAbility:";
 
