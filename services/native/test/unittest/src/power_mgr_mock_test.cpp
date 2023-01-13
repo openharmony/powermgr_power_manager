b944f1edf4a291f3915ead57a20d5209a8175a02
ShiJie <shijie20@huawei.com>
2023-01-13 10:56:38 +0800

    test: RebootDevice test
    Signed-off-by: ShiJie <shijie20@huawei.com>
    Change-Id: I970169b62527503c67f1fb7cacb09b22814b7288

--- services/native/test/unittest/src/power_mgr_mock_test.cpp
+++ services/native/test/unittest/src/power_mgr_mock_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -3123,5 +3123,25 @@ HWTEST_F (PowerMgrMockTest, PowerMgrUnittest091, TestSize.Level2)
     ResetMockAction();
     POWER_HILOGD(LABEL_TEST, "PowerMgrUnittest091:End.");
     GTEST_LOG_(INFO) << "PowerMgrUnittest091: end.";
+}
+
+/**
+ * @tc.name: PowerMgrUnittest092
+ * @tc.desc: test RebootDeviceForDeprecated by mock
+ * @tc.type: FUNC
+ */
+HWTEST_F (PowerMgrMockTest, PowerMgrUnittest001, TestSize.Level2)
+{
+    sleep(NEXT_WAIT_TIME_S);
+    GTEST_LOG_(INFO) << "PowerMgrUnittest092: start.";
+    POWER_HILOGD(LABEL_TEST, "PowerMgrUnittest092:Start.");
+
+    sptr<PowerMgrService> pms = DelayedSpSingleton<PowerMgrService>::GetInstance();
+    EXPECT_CALL(*g_powerAction, Reboot(std::string("test"))).Times(1);
+    pms->RebootDeviceForDeprecated(std::string("test"));
+
+    ResetMockAction();
+    POWER_HILOGD(LABEL_TEST, "PowerMgrUnittest092:End.");
+    GTEST_LOG_(INFO) << "PowerMgrUnittest092: end.";
 }
 }
--- services/native/test/unittest/src/reboot_test.cpp
+++ services/native/test/unittest/src/reboot_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -31,8 +31,8 @@ using namespace OHOS;
 using namespace std;
 namespace {
 /**
- * @tc.name: ShutDownDeviceTest001
- * @tc.desc: test ShutDownDevice in proxy
+ * @tc.name: RebootDeviceTest001
+ * @tc.desc: test RebootDevice in proxy
  * @tc.type: FUNC
  */
 HWTEST_F (PowerRebootTest, RebootDeviceTest001, TestSize.Level2)
@@ -44,5 +44,21 @@ HWTEST_F (PowerRebootTest, RebootDeviceTest001, TestSize.Level2)
         powerMgrClient.RebootDevice(string("RebootDeviceTest001"));
     }
     GTEST_LOG_(INFO) << "RebootDeviceTest001: RebootDevice end.";
+}
+
+/**
+ * @tc.name: RebootDeviceTest002
+ * @tc.desc: test RebootDeviceForDeprecated in proxy
+ * @tc.type: FUNC
+ */
+HWTEST_F (PowerRebootTest, RebootDeviceTest002, TestSize.Level2)
+{
+    sleep(SLEEP_WAIT_TIME_S);
+    GTEST_LOG_(INFO) << "RebootDeviceTest002: RebootDeviceForDeprecated start.";
+    auto& powerMgrClient = PowerMgrClient::GetInstance();
+    if (false) {
+        powerMgrClient.RebootDeviceForDeprecated(string("RebootDeviceTest002"));
+    }
+    GTEST_LOG_(INFO) << "RebootDeviceTest002: RebootDeviceForDeprecated end.";
 }
 }
