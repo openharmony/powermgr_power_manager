8efe5ac1ca2540bef6ceae41140fbf1f83f41780
qiuxiangdong <qiuxiangdong3@huawei.com>
2023-02-20 10:13:33 +0800

    style: Alarm clearing
    Signed-off-by: qiuxiangdong <qiuxiangdong3@huawei.com>
    Change-Id: Ic3c34cf020872a11c035cc5609792dc48800b407

--- frameworks/napi/runninglock/runninglock_napi.cpp
+++ frameworks/napi/runninglock/runninglock_napi.cpp
@@ -203,8 +203,8 @@ void RunningLockNapi::AsyncWork(napi_env& env, std::unique_ptr<AsyncCallbackInfo
 {
     napi_value resource = nullptr;
     napi_create_string_utf8(env, resourceName.c_str(), NAPI_AUTO_LENGTH, &resource);
-    napi_create_async_work(
-        env, nullptr, resource, execute, complete, reinterpret_cast<void*>(asyncInfo.get()), &asyncInfo->GetAsyncWork());
+    napi_create_async_work(env, nullptr, resource, execute, complete,
+        reinterpret_cast<void*>(asyncInfo.get()), &asyncInfo->GetAsyncWork());
     NAPI_CALL_RETURN_VOID(env, napi_queue_async_work(env, asyncInfo->GetAsyncWork()));
     asyncInfo.release();
 }
