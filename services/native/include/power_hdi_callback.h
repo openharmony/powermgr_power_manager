a157c4a66eb5c6a9693440e6f94d4ae2868d8371
yuanbo <yuanbo@huawei.com>
2023-02-21 14:51:19 +0800

    build: update power hdi to v1.1
    Signed-off-by: yuanbo <yuanbo@huawei.com>

--- services/native/include/power_hdi_callback.h
+++ services/native/include/power_hdi_callback.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,11 +17,11 @@
 #define POWERMGR_POWER_MANAGER_POWER_HDI_CALLBACK_H
 
 #include <cstdint>
-#include "v1_0/ipower_hdi_callback.h"
+#include "v1_1/ipower_hdi_callback.h"
 
 namespace OHOS {
 namespace PowerMgr {
-using namespace OHOS::HDI::Power::V1_0;
+using namespace OHOS::HDI::Power::V1_1;
 class PowerHdiCallback : public IPowerHdiCallback {
 public:
     virtual ~PowerHdiCallback() {}
