eb5661c3e54bb7b650e2c628f8c78fcbf3d808cf
zhang <zhangfengxi@huawei.com>
2023-03-10 11:18:57 +0800

    fix:Eliminate the dependency on the compilation path of bin hdf_core
    Signed-off-by: zhang <zhangfengxi@huawei.com>

--- services/native/src/actions/default/hdi_service_status_listener.h
+++ services/native/src/actions/default/hdi_service_status_listener.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -19,7 +19,7 @@
 #include <functional>
 
 #include <iservmgr_hdi.h>
-#include "hdf_io_service_if.h"
+#include "hdf_device_class.h"
 #include "hdf_service_status.h"
 
 namespace OHOS {
