ec9ba4e1864482e41d44f309f761d98199bcb808
lyj_love_code <liangyujian2@huawei.com>
2023-03-18 18:13:07 +0800

    fix: fix the name format of hisysevent
    Signed-off-by: lyj_love_code <liangyujian2@huawei.com>

--- services/native/src/running_lock_mgr.cpp
+++ services/native/src/running_lock_mgr.cpp
@@ -637,7 +637,7 @@ void RunningLockMgr::NotifyHiView(RunningLockChangedType changeType,
     string name = lockInner.GetRunningLockName();
     const int logLevel = 2;
     const string &tag = runninglockNotifyStr_.at(changeType);
-    HiSysEventWrite(HiviewDFX::HiSysEvent::Domain::POWER, "POWER_RUNNINGLOCK",
+    HiSysEventWrite(HiviewDFX::HiSysEvent::Domain::POWER, "RUNNINGLOCK",
         HiviewDFX::HiSysEvent::EventType::STATISTIC,
         "PID", pid, "UID", uid, "STATE", state, "TYPE", type, "NAME", name,
         "LOG_LEVEL", logLevel, "TAG", tag, "MESSAGE", msg);
