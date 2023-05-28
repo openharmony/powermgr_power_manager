8f8449c5d0d194b9b28885423d2f4c342cfa7e2e
ShiJie <shijie20@huawei.com>
2023-05-28 22:05:14 +0800

    test: add sutb fuzz
    Signed-off-by: ShiJie <shijie20@huawei.com>
    Change-Id: Ie9020a89135a5c9ea4a0dd085cf124b991e0fa64

--- test/fuzztest/power_fuzzer/power_fuzzer.cpp
+++ test/fuzztest/power_fuzzer/power_fuzzer.cpp
@@ -17,327 +17,290 @@
 
 #include "power_fuzzer.h"
 
+#include <cstddef>
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
 #include <random>
-#include <cstddef>
+
+#include "actions/idevice_power_action.h"
+#include "actions/idevice_state_action.h"
+#include "message_parcel.h"
+#include "power_mgr_client.h"
+#include "power_mgr_service.h"
+#include "power_state_machine_info.h"
+#include "running_lock_action.h"
 #include "securec.h"
 
-#include "power_mgr_client.h"
-
+using namespace OHOS;
 using namespace OHOS::PowerMgr;
 using namespace std;
 
+namespace OHOS {
+namespace PowerMgr {
+class FuzzPowerAction : public IDevicePowerAction {
+public:
+    FuzzPowerAction() = default;
+    virtual ~FuzzPowerAction() = default;
+    virtual void Reboot([[maybe_unused]] const std::string& reason) {};
+    virtual void Shutdown([[maybe_unused]] const std::string& reason) {};
+};
+
+class FuzzStateAction : public IDeviceStateAction {
+public:
+    FuzzStateAction() = default;
+    virtual ~FuzzStateAction() = default;
+    virtual void Suspend([[maybe_unused]] int64_t callTimeMs, [[maybe_unused]] SuspendDeviceType type,
+        [[maybe_unused]] uint32_t flags) {};
+    virtual void ForceSuspend() {};
+    virtual void Wakeup([[maybe_unused]] int64_t callTimeMs, [[maybe_unused]] WakeupDeviceType type,
+        [[maybe_unused]] const std::string& details, [[maybe_unused]] const std::string& pkgName) {};
+    virtual void RefreshActivity([[maybe_unused]] int64_t callTimeMs, [[maybe_unused]] UserActivityType type,
+        [[maybe_unused]] uint32_t flags) {};
+    virtual DisplayState GetDisplayState()
+    {
+        return DisplayState::DISPLAY_OFF;
+    };
+    virtual uint32_t SetDisplayState([[maybe_unused]] DisplayState state,
+        [[maybe_unused]] StateChangeReason reason = StateChangeReason::STATE_CHANGE_REASON_UNKNOWN)
+    {
+        return 0;
+    }
+    virtual uint32_t GoToSleep([[maybe_unused]] const std::function<void()> onSuspend,
+        [[maybe_unused]] const std::function<void()> onWakeup, [[maybe_unused]] bool force)
+    {
+        return 0;
+    }
+    virtual void RegisterCallback([[maybe_unused]] std::function<void(uint32_t)>& callback) {};
+};
+} // namespace PowerMgr
+} // namespace OHOS
+
 namespace {
 auto& g_powerMgrClient = PowerMgrClient::GetInstance();
-constexpr int32_t SIZE = 1;
-constexpr int32_t INDEX_0 = 0;
+sptr<PowerMgrService> g_service = nullptr;
+const int32_t REWIND_READ_DATA = 0;
+} // namespace
+
+static int32_t GetInt32(const uint8_t* data, size_t size)
+{
+    int32_t value = 0;
+    if (size < sizeof(value)) {
+        return value;
+    }
+    if (memcpy_s(&value, sizeof(value), data, sizeof(value)) != EOK) {
+        return value;
+    }
+    return value;
 }
 
-static void SuspendDevice(const uint8_t* data)
+static void SuspendDevice(const uint8_t* data, size_t size)
 {
-    int32_t type[1];
-    int32_t idSize = 4;
-    if ((memcpy_s(type, sizeof(type), data, idSize)) != EOK) {
-        return;
-    }
-
-    g_powerMgrClient.SuspendDevice(static_cast<SuspendDeviceType>(type[0]));
+    int32_t reason = GetInt32(data, size);
+    g_powerMgrClient.SuspendDevice(static_cast<SuspendDeviceType>(reason), static_cast<bool>(reason));
 }
 
-static void WakeupDevice(const uint8_t* data)
+static void WakeupDevice(const uint8_t* data, size_t size)
 {
-    int32_t type[1];
-    int32_t idSize = 4;
-    if ((memcpy_s(type, sizeof(type), data, idSize)) != EOK) {
+    if (data == nullptr || size == 0) {
         return;
     }
-
-    g_powerMgrClient.WakeupDevice(static_cast<WakeupDeviceType>(type[0]));
+    int32_t reason = GetInt32(data, size);
+    std::string detail(reinterpret_cast<const char*>(data), size);
+    g_powerMgrClient.WakeupDevice(static_cast<WakeupDeviceType>(reason), detail);
 }
 
-static void RefreshActivity(const uint8_t* data)
+static void RefreshActivity(const uint8_t* data, size_t size)
 {
-    int32_t type[1];
-    int32_t idSize = 4;
-    if ((memcpy_s(type, sizeof(type), data, idSize)) != EOK) {
-        return;
-    }
-
-    g_powerMgrClient.RefreshActivity(static_cast<UserActivityType>(type[0]));
+    int32_t type = GetInt32(data, size);
+    g_powerMgrClient.RefreshActivity(static_cast<UserActivityType>(type));
 }
 
-static void IsScreenOn(const uint8_t* data)
+static void IsScreenOn([[maybe_unused]] const uint8_t* data, [[maybe_unused]] size_t size)
 {
-    int32_t type[1];
-    int32_t idSize = 4;
-    if ((memcpy_s(type, sizeof(type), data, idSize)) != EOK) {
-        return;
-    }
-
     g_powerMgrClient.IsScreenOn();
 }
 
-static void GetState(const uint8_t* data)
+static void GetState([[maybe_unused]] const uint8_t* data, [[maybe_unused]] size_t size)
 {
-    int32_t type[1];
-    int32_t idSize = 4;
-    if ((memcpy_s(type, sizeof(type), data, idSize)) != EOK) {
-        return;
-    }
-
     g_powerMgrClient.GetState();
 }
 
-static void SetDeviceMode(const uint8_t* data)
+static void SetDeviceMode(const uint8_t* data, size_t size)
 {
-    int32_t type[1];
-    int32_t idSize = 4;
-    if ((memcpy_s(type, sizeof(type), data, idSize)) != EOK) {
-        return;
-    }
-
-    g_powerMgrClient.SetDeviceMode(static_cast<PowerMode>(type[0]));
+    int32_t mode = GetInt32(data, size);
+    g_powerMgrClient.SetDeviceMode(static_cast<PowerMode>(mode));
 }
 
-static void GetDeviceMode(const uint8_t* data)
+static void GetDeviceMode([[maybe_unused]] const uint8_t* data, [[maybe_unused]] size_t size)
 {
-    int32_t type[1];
-    int32_t idSize = 4;
-    if ((memcpy_s(type, sizeof(type), data, idSize)) != EOK) {
-        return;
-    }
-
     g_powerMgrClient.GetDeviceMode();
 }
 
-static void CreateRunningLock(const uint8_t* data)
+static void CreateRunningLock(const uint8_t* data, size_t size)
 {
-    int32_t type[1];
-    int32_t idSize = 4;
-    if ((memcpy_s(type, sizeof(type), data, idSize)) != EOK) {
+    if (data == nullptr || size == 0) {
         return;
     }
-
-    g_powerMgrClient.CreateRunningLock("FUZZ", static_cast <RunningLockType>(type[0]));
+    int32_t type = GetInt32(data, size);
+    std::string name(reinterpret_cast<const char*>(data), size);
+    g_powerMgrClient.CreateRunningLock(name, static_cast<RunningLockType>(type));
 }
 
-static void RegisterPowerStateCallback(const uint8_t* data)
+static void RegisterPowerStateCallback(const uint8_t* data, size_t size)
 {
-    static OHOS::sptr<IPowerStateCallback> callback;
-    int32_t idSize = 4;
-    if ((memcpy_s(callback, sizeof(callback), data, idSize)) != EOK) {
+    OHOS::sptr<IPowerStateCallback> callback;
+    if ((memcpy_s(callback, sizeof(callback), data, sizeof(callback))) != EOK) {
         return;
     }
-
     g_powerMgrClient.RegisterPowerStateCallback(callback);
     g_powerMgrClient.UnRegisterPowerStateCallback(callback);
 }
 
-static void RegisterPowerModeCallback(const uint8_t* data)
+static void RegisterPowerModeCallback(const uint8_t* data, size_t size)
 {
-    static OHOS::sptr<IPowerModeCallback> callback;
-    int32_t idSize = 4;
-    if ((memcpy_s(callback, sizeof(callback), data, idSize)) != EOK) {
+    OHOS::sptr<IPowerModeCallback> callback;
+    if ((memcpy_s(callback, sizeof(callback), data, sizeof(callback))) != EOK) {
         return;
     }
-
     g_powerMgrClient.RegisterPowerModeCallback(callback);
     g_powerMgrClient.UnRegisterPowerModeCallback(callback);
 }
 
-static void RegisterShutdownCallback(const uint8_t* data)
+static void RegisterShutdownCallback(const uint8_t* data, size_t size)
 {
-    static OHOS::sptr<IShutdownCallback> callback;
-    int32_t idSize = 4;
-    if ((memcpy_s(callback, sizeof(callback), data, idSize)) != EOK) {
+    OHOS::sptr<IShutdownCallback> callback;
+    if ((memcpy_s(callback, sizeof(callback), data, sizeof(callback))) != EOK) {
         return;
     }
-    uint32_t intPriority[SIZE];
-    if (memcpy_s(intPriority, sizeof(intPriority), data, idSize) != EOK) {
-        return;
-    }
-    IShutdownCallback::ShutdownPriority priority =
-        static_cast<IShutdownCallback::ShutdownPriority>(intPriority[INDEX_0]);
+    int32_t type = GetInt32(data, size);
+    IShutdownCallback::ShutdownPriority priority = static_cast<IShutdownCallback::ShutdownPriority>(type);
     g_powerMgrClient.RegisterShutdownCallback(callback, priority);
     g_powerMgrClient.UnRegisterShutdownCallback(callback);
 }
 
-static void ProxyRunningLock(const uint8_t* data)
+static void ProxyRunningLock(const uint8_t* data, size_t size)
 {
-    int32_t type[1];
-    int32_t idSize = 4;
-    if ((memcpy_s(type, sizeof(type), data, idSize)) != EOK) {
-        return;
-    }
-
-    bool isProxied = static_cast <bool>(type[0]);
-    pid_t pid = static_cast <pid_t>(type[0]);
-    pid_t uid = static_cast <pid_t>(type[0]);
+    int32_t type = GetInt32(data, size);
+    bool isProxied = static_cast<bool>(type);
+    pid_t pid = static_cast<pid_t>(type);
+    pid_t uid = static_cast<pid_t>(type);
     g_powerMgrClient.ProxyRunningLock(isProxied, pid, uid);
 }
 
-static void Lock(const uint8_t* data)
+static void Lock(const uint8_t* data, size_t size)
 {
-    int32_t type[1];
-    int32_t idSize = 4;
-    if ((memcpy_s(type, sizeof(type), data, idSize)) != EOK) {
+    if (data == nullptr || size == 0) {
         return;
     }
-
-    auto fuzzLock = g_powerMgrClient.CreateRunningLock("FUZZ", static_cast <RunningLockType>(type[0]));
+    int32_t type = GetInt32(data, size);
+    std::string name(reinterpret_cast<const char*>(data), size);
+    auto fuzzLock = g_powerMgrClient.CreateRunningLock(name, static_cast<RunningLockType>(type));
     if (fuzzLock == nullptr) {
         return;
     }
-
-    fuzzLock -> Lock();
+    int32_t timeout = GetInt32(data, size);
+    fuzzLock->Lock(timeout);
 }
 
-static void UnLock(const uint8_t* data)
+static void UnLock(const uint8_t* data, size_t size)
 {
-    int32_t type[1];
-    int32_t idSize = 4;
-    if ((memcpy_s(type, sizeof(type), data, idSize)) != EOK) {
+    if (data == nullptr || size == 0) {
         return;
     }
-
-    auto fuzzLock = g_powerMgrClient.CreateRunningLock("FUZZ", static_cast <RunningLockType>(type[0]));
+    int32_t type = GetInt32(data, size);
+    std::string name(reinterpret_cast<const char*>(data), size);
+    auto fuzzLock = g_powerMgrClient.CreateRunningLock(name, static_cast<RunningLockType>(type));
     if (fuzzLock == nullptr) {
         return;
     }
-
-    fuzzLock -> Lock();
-    fuzzLock -> UnLock();
+    fuzzLock->UnLock();
 }
 
-static void OverrideScreenOffTime(const uint8_t* data)
+static void OverrideScreenOffTime(const uint8_t* data, size_t size)
 {
-    int64_t type[1];
-    int32_t idSize = 4;
-    if ((memcpy_s(type, sizeof(type), data, idSize)) != EOK) {
-        return;
-    }
-
-    g_powerMgrClient.OverrideScreenOffTime(type[0]);
+    int32_t timeout = GetInt32(data, size);
+    g_powerMgrClient.OverrideScreenOffTime(timeout);
 }
 
-static void RestoreScreenOffTime(const uint8_t* data)
+static void RestoreScreenOffTime([[maybe_unused]] const uint8_t* data, [[maybe_unused]] size_t size)
 {
-    int32_t type[1];
-    int32_t idSize = 4;
-    if ((memcpy_s(type, sizeof(type), data, idSize)) != EOK) {
-        return;
-    }
-
     g_powerMgrClient.RestoreScreenOffTime();
 }
 
-static void IsRunningLockTypeSupported(const uint8_t* data)
+static void IsRunningLockTypeSupported(const uint8_t* data, size_t size)
 {
-    uint32_t type[SIZE];
-    int32_t idSize = 4;
-    if ((memcpy_s(type, sizeof(type), data, idSize)) != EOK) {
+    int32_t type = GetInt32(data, size);
+    g_powerMgrClient.IsRunningLockTypeSupported(static_cast<RunningLockType>(type));
+}
+
+static void SetDisplaySuspend(const uint8_t* data, size_t size)
+{
+    int32_t suspend = GetInt32(data, size);
+    g_powerMgrClient.SetDisplaySuspend(static_cast<bool>(suspend));
+}
+
+static void TestPowerServiceStub(const uint8_t* data, size_t size)
+{
+    uint32_t code;
+    if (size < sizeof(code)) {
+        return;
+    }
+    if (memcpy_s(&code, sizeof(code), data, sizeof(code)) != EOK) {
         return;
     }
 
-    g_powerMgrClient.IsRunningLockTypeSupported(static_cast<RunningLockType>(type[INDEX_0]));
-}
-
-static void SetDisplaySuspend(const uint8_t* data)
-{
-    char type[SIZE];
-    int32_t idSize = 1;
-    if ((memcpy_s(type, sizeof(type), data, idSize)) != EOK) {
-        return;
+    MessageParcel datas;
+    datas.WriteInterfaceToken(PowerMgrService::GetDescriptor());
+    datas.WriteBuffer(data, size);
+    datas.RewindRead(REWIND_READ_DATA);
+    MessageParcel reply;
+    MessageOption option;
+    if (g_service == nullptr) {
+        g_service = DelayedSpSingleton<PowerMgrService>::GetInstance();
+        g_service->OnStart();
+        auto powerAction = new FuzzPowerAction();
+        auto powerState = new FuzzStateAction();
+        auto shutdownState = new FuzzStateAction();
+        auto lockAction = new RunningLockAction();
+        g_service->EnableMock(powerState, shutdownState, powerAction, lockAction);
     }
-
-    g_powerMgrClient.SetDisplaySuspend(static_cast<bool>(type[INDEX_0]));
+    g_service->OnRemoteRequest(code, datas, reply, option);
 }
 
-static int32_t randNum()
-{
-    std::random_device rd;
-    std::default_random_engine engine(rd());
-    std::uniform_int_distribution<int32_t> randomNum(static_cast<int32_t>(ApiNumber::NUM_ZERO),
-        static_cast<int32_t>(ApiNumber::NUM_END) - 1);
-    return randomNum(engine);
-}
+static std::vector<std::function<void(const uint8_t*, size_t)>> fuzzFunc = {
+    &SuspendDevice,
+    &WakeupDevice,
+    &RefreshActivity,
+    &IsScreenOn,
+    &GetState,
+    &SetDeviceMode,
+    &GetDeviceMode,
+    &CreateRunningLock,
+    &RegisterPowerStateCallback,
+    &RegisterPowerModeCallback,
+    &RegisterShutdownCallback,
+    &ProxyRunningLock,
+    &Lock,
+    &UnLock,
+    &OverrideScreenOffTime,
+    &RestoreScreenOffTime,
+    &IsRunningLockTypeSupported,
+    &SetDisplaySuspend
+};
 
 namespace OHOS {
 bool DoSomethingInterestingWithMyAPI(const uint8_t* data, size_t size)
 {
-    int32_t idSize = 4;
-    int32_t cond[1];
-    if (static_cast<int32_t>(size) > idSize) {
-        if ((memcpy_s(cond, sizeof(cond), data, idSize)) != EOK) {
-            return false;
-        }
-        int32_t number = randNum();
-
-        switch (static_cast<ApiNumber>(number)) {
-            case ApiNumber::NUM_ZERO:
-                SuspendDevice(data);
-                break;
-            case ApiNumber::NUM_ONE:
-                WakeupDevice(data);
-                break;
-            case ApiNumber::NUM_TWO:
-                RefreshActivity(data);
-                break;
-            case ApiNumber::NUM_THREE:
-                IsScreenOn(data);
-                break;
-            case ApiNumber::NUM_FOUR:
-                GetState(data);
-                break;
-            case ApiNumber::NUM_EIGHT:
-                SetDeviceMode(data);
-                break;
-            case ApiNumber::NUM_NINE:
-                GetDeviceMode(data);
-                break;
-            case ApiNumber::NUM_TEN:
-                CreateRunningLock(data);
-                break;
-            case ApiNumber::NUM_ELEVEN:
-                RegisterPowerStateCallback(data);
-                break;
-            case ApiNumber::NUM_THIRTEEN:
-                RegisterPowerModeCallback(data);
-                break;
-            case ApiNumber::NUM_FIFTEEN:
-                Lock(data);
-                break;
-            case ApiNumber::NUM_SIXTEEN:
-                UnLock(data);
-                break;
-            case ApiNumber::NUM_SEVENTEEN:
-                OverrideScreenOffTime(data);
-                break;
-            case ApiNumber::NUM_EIGHTEEN:
-                RestoreScreenOffTime(data);
-                break;
-            case ApiNumber::NUM_NINETEEN:
-                IsRunningLockTypeSupported(data);
-                break;
-            case ApiNumber::NUM_TWENTY:
-                SetDisplaySuspend(data);
-                break;
-            case ApiNumber::NUM_TWENTY_ONE:
-                RegisterShutdownCallback(data);
-                break;
-            case ApiNumber::NUM_TWENTY_TWO:
-                ProxyRunningLock(data);
-                break;
-            default:
-                break;
-        }
-    }
-
+    std::random_device rd;
+    std::default_random_engine engine(rd());
+    std::uniform_int_distribution<int32_t> randomNum(0, fuzzFunc.size() - 1);
+    int32_t number = randomNum(engine);
+    fuzzFunc[number](data, size);
+    TestPowerServiceStub(data, size);
     return true;
 }
-}
+} // namespace OHOS
 
 /* Fuzzer entry point */
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
@@ -346,4 +309,3 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
     OHOS::DoSomethingInterestingWithMyAPI(data, size);
     return 0;
 }
-
--- test/fuzztest/power_fuzzer/power_fuzzer.h
+++ test/fuzztest/power_fuzzer/power_fuzzer.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -19,32 +19,5 @@
 #define POWER_FUZZER_TEST_H
 
 #define FUZZ_PROJECT_NAME "power_fuzzer"
-
-enum class ApiNumber {
-    NUM_ZERO = 0,
-    NUM_ONE,
-    NUM_TWO,
-    NUM_THREE,
-    NUM_FOUR,
-    NUM_FIVE,
-    NUM_SIX,
-    NUM_SEVEN,
-    NUM_EIGHT,
-    NUM_NINE,
-    NUM_TEN,
-    NUM_ELEVEN,
-    NUM_TWELVE,
-    NUM_THIRTEEN,
-    NUM_FOURTEEN,
-    NUM_FIFTEEN,
-    NUM_SIXTEEN,
-    NUM_SEVENTEEN,
-    NUM_EIGHTEEN,
-    NUM_NINETEEN,
-    NUM_TWENTY,
-    NUM_TWENTY_ONE,
-    NUM_TWENTY_TWO,
-    NUM_END
-};
 
 #endif
