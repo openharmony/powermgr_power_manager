2434bd7089b7437f0459d917ce96436113c8c7d0
suozhipeng <suozhipeng@huawei.com>
2023-10-10 04:53:30 +0000

    feat: shutdown dialog vibration
    Signed-off-by: suozhipeng <suozhipeng@huawei.com>

---
+++ utils/vibrator/src/power_vibrator.cpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "power_vibrator.h"
+
+#include <string>
+#include "power_log.h"
+#include "vibrator_agent.h"
+#include "vibrator_source_parser.h"
+
+namespace OHOS {
+namespace PowerMgr {
+namespace {
+const int32_t VIBRATOR_SUCCESS = 0;
+}
+std::mutex PowerVibrator::mutex_;
+std::shared_ptr<PowerVibrator> PowerVibrator::instance_ = nullptr;
+
+void PowerVibrator::InitConfig(std::string& etcPath, std::string& vendorPath, std::string& systemPath)
+{
+    std::shared_ptr<VibratorSourceParser> parser = std::make_shared<VibratorSourceParser>();
+    sourceList_ = parser->ParseSources(etcPath, vendorPath, systemPath);
+}
+
+void PowerVibrator::StartVibrator(std::string &scene)
+{
+    VibratorSource source;
+    for (VibratorSource &src : sourceList_) {
+        if (src.GetScene() == scene) {
+            source = src;
+            break;
+        }
+    }
+    if (!source.IsEnable() || source.GetType() == "") {
+        POWER_HILOGI(COMP_UTILS, "%{public}s do not need vibrator", scene.c_str());
+        return;
+    }
+    int32_t ret = Sensors::StartVibrator(source.GetType().c_str());
+    if (ret != VIBRATOR_SUCCESS) {
+        POWER_HILOGE(COMP_UTILS, "%{public}s vibrator failed", scene.c_str());
+        return;
+    }
+}
+
+std::shared_ptr<PowerVibrator> PowerVibrator::GetInstance()
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+    if (instance_ != nullptr) {
+        return instance_;
+    }
+    instance_ = std::make_shared<PowerVibrator>();
+    return instance_;
+}
+} // namespace PowerMgr
+} // namespace OHOS
