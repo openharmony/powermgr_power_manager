acf803564c7c2d41c2e25d52d32c345bfe6673c7
volcano6322 <wangcan61@huawei.com>
2024-04-18 20:21:48 +0800

    fix: 电源服务激活常亮锁时不做亮屏操作
    Signed-off-by: volcano6322 <wangcan61@huawei.com>

--- services/native/src/running_lock_mgr.cpp
+++ services/native/src/running_lock_mgr.cpp
@@ -95,13 +95,9 @@ void RunningLockMgr::InitLocksTypeScreen()
                 return;
             }
             if (active) {
-                POWER_HILOGI(FEATURE_RUNNING_LOCK, "[UL_POWER] RUNNINGLOCK_SCREEN active");
-                SystemSuspendController::GetInstance().Wakeup();
-                FFRTTask task = [this, stateMachine] {
-                    stateMachine->SetState(PowerState::AWAKE,
-                        StateChangeReason::STATE_CHANGE_REASON_RUNNING_LOCK);
-                };
-                FFRTUtils::SubmitTask(task);
+                POWER_HILOGI(FEATURE_RUNNING_LOCK,
+                    "[UL_POWER] RUNNINGLOCK_SCREEN active, and the  currrent power state = %{public}d",
+                    stateMachine->GetState());
             } else {
                 POWER_HILOGI(FEATURE_RUNNING_LOCK, "[UL_POWER] RUNNINGLOCK_SCREEN inactive");
                 if (stateMachine->GetState() == PowerState::AWAKE) {
