4da6fb7a4fe443f4906e423973329b14e4cb8d9b
z30053694 <zhangchen190@huawei.com>
2024-05-27 17:21:42 +0800

    fix: apply suggestion from zhangwei
    Signed-off-by: z30053694 <zhangchen190@huawei.com>
    Change-Id: I875e79df8fbb55dfbf420d7fab2f4ca82b588fcd

--- services/native/include/power_mgr_service.h
+++ services/native/include/power_mgr_service.h
@@ -58,6 +58,7 @@ public:
     virtual PowerErrors SuspendDevice(int64_t callTimeMs, SuspendDeviceType reason, bool suspendImmed) override;
     virtual PowerErrors WakeupDevice(int64_t callTimeMs, WakeupDeviceType reason, const std::string& details) override;
     virtual bool RefreshActivity(int64_t callTimeMs, UserActivityType type, bool needChangeBacklight) override;
+    bool RefreshActivityInner(int64_t callTimeMs, UserActivityType type, bool needChangeBacklight);
     virtual bool OverrideScreenOffTime(int64_t timeout) override;
     virtual bool RestoreScreenOffTime() override;
     virtual PowerState GetState() override;
