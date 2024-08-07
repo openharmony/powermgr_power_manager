a9ffa585046fad6e8d26b34af0dc3853a982dd2e
chenjunwu <chenjunwu4@huawei.com>
2024-08-07 17:41:00 +0800

    fix: multi-user adaptation
    Signed-off-by: chenjunwu <chenjunwu4@huawei.com>

--- services/native/src/wakeup/wakeup_controller.h
+++ services/native/src/wakeup/wakeup_controller.h
@@ -45,6 +45,7 @@ public:
     void Init();
     void Cancel();
     void RegisterSettingsObserver();
+    void UnregisterSettingsObserver();
     void ExecWakeupMonitorByReason(WakeupDeviceType reason);
     void Wakeup();
     void NotifyDisplayActionDone(uint32_t event);
