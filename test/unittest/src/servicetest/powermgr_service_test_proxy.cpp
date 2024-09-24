f11fac82ba6a7a4118f15819262325dd277c935e
杨鹏昊 <yangpenghao1@h-partners.com>
2024-09-24 09:04:46 +0000

    fix: Increase judgment
    Signed-off-by: 杨鹏昊 <yangpenghao1@h-partners.com>

--- test/unittest/src/servicetest/powermgr_service_test_proxy.cpp
+++ test/unittest/src/servicetest/powermgr_service_test_proxy.cpp
@@ -853,12 +853,12 @@ std::string PowerMgrServiceTestProxy::ShellDump(const std::vector<std::string>& 
         POWER_HILOGE(COMP_FWK, "Write descriptor failed");
         return result;
     }
+
+    data.WriteUint32(argc);
     if (argc > args.size()) {
         POWER_HILOGE(COMP_FWK, "argc is greater than args size!");
         return result;
     }
-
-    data.WriteUint32(argc);
     for (uint32_t i = 0; i < argc; i++) {
         data.WriteString(args[i]);
     }
