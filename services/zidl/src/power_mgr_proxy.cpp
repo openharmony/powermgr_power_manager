96848834e7007c8bb7078b3a5de12e17593b6bb8
杨鹏昊 <yangpenghao1@h-partners.com>
2024-09-24 09:14:48 +0000

    fix: Increase judgment
    Signed-off-by: 杨鹏昊 <yangpenghao1@h-partners.com>

--- services/zidl/src/power_mgr_proxy.cpp
+++ services/zidl/src/power_mgr_proxy.cpp
@@ -1204,7 +1204,7 @@ std::string PowerMgrProxy::ShellDump(const std::vector<std::string>& args, uint3
     if (argc > args.size()) {
         POWER_HILOGE(COMP_FWK, "argc is greater than args size!");
         return result;
-    }    
+    }
     for (uint32_t i = 0; i < argc; i++) {
         data.WriteString(args[i]);
     }
