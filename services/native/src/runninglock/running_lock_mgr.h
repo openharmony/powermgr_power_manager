ab5029f441283516b6b39a70bb73314ff103b65b
z30053694 <zhangchen190@huawei.com>
2024-11-01 14:32:25 +0800

    fix: async wakeup after deactivating multi screen collaboration
    Signed-off-by: z30053694 <zhangchen190@huawei.com>
    Change-Id: Id9c4f3315b1d3bc5e2107de2543403981250c560

--- services/native/src/runninglock/running_lock_mgr.h
+++ services/native/src/runninglock/running_lock_mgr.h
@@ -73,6 +73,7 @@ public:
     void UnlockInnerByProxy(const sptr<IRemoteObject>& remoteObj, std::shared_ptr<RunningLockInner>& lockInner);
     void ResetRunningLocks();
     bool IsUsed(const sptr<IRemoteObject>& remoteObj);
+    void AsyncWakeup();
     static constexpr uint32_t CHECK_TIMEOUT_INTERVAL_MS = 60 * 1000;
 #ifdef HAS_SENSORS_SENSOR_PART
     void SetProximity(uint32_t status);
