7b7607c33a00f8ff77a73877d33fae0934d6791a
wangyantian <wangyantian@huawei.com>
2024-11-11 16:07:53 +0800

    fix: add set compare
    Signed-off-by: wangyantian <wangyantian@huawei.com>

--- services/native/src/suspend/suspend_controller.h
+++ services/native/src/suspend/suspend_controller.h
@@ -93,8 +93,8 @@ private:
     void HandleHibernate(SuspendDeviceType reason);
     void HandleShutdown(SuspendDeviceType reason);
 
-    void TriggerSyncSleepCallbackInner(std::set<sptr<ISyncSleepCallback>>& callbacks, const std::string& priority,
-        bool isWakeup);
+    void TriggerSyncSleepCallbackInner(
+        SleepCallbackHolder::SleepCallbackContainerType& callbacks, const std::string& priority, bool isWakeup);
     static constexpr int32_t FORCE_SLEEP_DELAY_MS = 8000;
     void SuspendWhenScreenOff(SuspendDeviceType reason, uint32_t action, uint32_t delay);
     std::vector<SuspendSource> sourceList_;
