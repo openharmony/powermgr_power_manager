6736c0ba9742452a0e85ea7a6afc4eee34d4ee11
guochengying <346447065@qq.com>
2025-03-13 11:24:02 +0800

    fix: refactor duplicate code
    Signed-off-by: guochengying <346447065@qq.com>

--- services/native/src/power_mgr_notify.cpp
+++ services/native/src/power_mgr_notify.cpp
@@ -31,26 +31,36 @@ void PowerMgrNotify::RegisterPublishEvents()
     if (publishInfo_ != nullptr) {
         return;
     }
-    publishInfo_ = new (std::nothrow)CommonEventPublishInfo();
+    publishInfo_ = new (std::nothrow) CommonEventPublishInfo();
     if (publishInfo_ != nullptr) {
         publishInfo_->SetOrdered(false);
     }
-    screenOffWant_ = new (std::nothrow)IntentWant();
+    screenOffWant_ = new (std::nothrow) IntentWant();
     if (screenOffWant_ != nullptr) {
         screenOffWant_->SetAction(CommonEventSupport::COMMON_EVENT_SCREEN_OFF);
     }
-    screenOnWant_ = new (std::nothrow)IntentWant();
+    screenOnWant_ = new (std::nothrow) IntentWant();
     if (screenOnWant_ != nullptr) {
         screenOnWant_->SetAction(CommonEventSupport::COMMON_EVENT_SCREEN_ON);
     }
 #ifdef POWER_MANAGER_ENABLE_FORCE_SLEEP_BROADCAST
-    enterForceSleepWant_ = new (std::nothrow)IntentWant();
+    enterForceSleepWant_ = new (std::nothrow) IntentWant();
     if (enterForceSleepWant_ != nullptr) {
         enterForceSleepWant_->SetAction(CommonEventSupport::COMMON_EVENT_ENTER_FORCE_SLEEP);
     }
-    exitForceSleepWant_ = new (std::nothrow)IntentWant();
+    exitForceSleepWant_ = new (std::nothrow) IntentWant();
     if (exitForceSleepWant_ != nullptr) {
         exitForceSleepWant_->SetAction(CommonEventSupport::COMMON_EVENT_EXIT_FORCE_SLEEP);
+    }
+#endif
+#ifdef POWER_MANAGER_POWER_ENABLE_S4
+    enterHibernateWant_ = new (std::nothrow) IntentWant();
+    if (enterHibernateWant_ != nullptr) {
+        enterHibernateWant_->SetAction(CommonEventSupport::COMMON_EVENT_ENTER_HIBERNATE);
+    }
+    exitHibernateWant_ = new (std::nothrow) IntentWant();
+    if (exitHibernateWant_ != nullptr) {
+        exitHibernateWant_->SetAction(CommonEventSupport::COMMON_EVENT_EXIT_HIBERNATE);
     }
 #endif
 }
@@ -119,36 +129,26 @@ void PowerMgrNotify::PublishExitForceSleepEvents(int64_t eventTime)
 #ifdef POWER_MANAGER_POWER_ENABLE_S4
 void PowerMgrNotify::PublishEnterHibernateEvent(int64_t eventTime)
 {
-    std::string action = CommonEventSupport::COMMON_EVENT_ENTER_HIBERNATE;
-    POWER_HILOGI(FEATURE_SUSPEND, "[UL_POWER] Start to publish event %{public}s at %{public}lld", action.c_str(),
-        static_cast<long long>(eventTime));
-    CommonEventPublishInfo publishInfo;
-    publishInfo.SetOrdered(false);
-    IntentWant enterHibernateWant;
-    enterHibernateWant.SetAction(action);
-    CommonEventData event(enterHibernateWant);
-    if (!CommonEventManager::PublishCommonEvent(event, publishInfo, nullptr)) {
-        POWER_HILOGE(FEATURE_WAKEUP, "[UL_POWER] Publish event %{public}s fail", action.c_str());
+    if (enterHibernateWant_ == nullptr) {
+        POWER_HILOGE(COMP_SVC, "%{public}s: Invalid parameter", __func__);
         return;
     }
-    POWER_HILOGI(FEATURE_SUSPEND, "[UL_POWER] Publish event %{public}s done", action.c_str());
+    POWER_HILOGI(FEATURE_SUSPEND, "[UL_POWER] Start to publish event %{public}s at %{public}lld",
+        enterHibernateWant_->GetAction().c_str(), static_cast<long long>(eventTime));
+    PublishEvents(eventTime, enterHibernateWant_);
+    POWER_HILOGI(FEATURE_SUSPEND, "[UL_POWER] Publish event %{public}s done", enterHibernateWant_->GetAction().c_str());
 }
 
 void PowerMgrNotify::PublishExitHibernateEvent(int64_t eventTime)
 {
-    std::string action = CommonEventSupport::COMMON_EVENT_EXIT_HIBERNATE;
-    POWER_HILOGI(FEATURE_SUSPEND, "[UL_POWER] Start to publish event %{public}s at %{public}lld", action.c_str(),
-        static_cast<long long>(eventTime));
-    CommonEventPublishInfo publishInfo;
-    publishInfo.SetOrdered(false);
-    IntentWant exitHibernateWant;
-    exitHibernateWant.SetAction(action);
-    CommonEventData event(exitHibernateWant);
-    if (!CommonEventManager::PublishCommonEvent(event, publishInfo, nullptr)) {
-        POWER_HILOGE(FEATURE_WAKEUP, "[UL_POWER] Publish event %{public}s fail", action.c_str());
+    if (exitHibernateWant_ == nullptr) {
+        POWER_HILOGE(COMP_SVC, "%{public}s: Invalid parameter", __func__);
         return;
     }
-    POWER_HILOGI(FEATURE_SUSPEND, "[UL_POWER] Publish event %{public}s done", action.c_str());
+    POWER_HILOGI(FEATURE_WAKEUP, "[UL_POWER] Start to publish event %{public}s at %{public}lld",
+        exitHibernateWant_->GetAction().c_str(), static_cast<long long>(eventTime));
+    PublishEvents(eventTime, exitHibernateWant_);
+    POWER_HILOGI(FEATURE_WAKEUP, "[UL_POWER] Publish event %{public}s done", exitHibernateWant_->GetAction().c_str());
 }
 #endif
 } // namespace PowerMgr
