f48f6be793fc6ec40e10538a823d1aa3e2ce7739
zjl_goldenglow <zhangjiale43@huawei.com>
2025-09-16 21:46:47 +0800

    update: 更新文件 setting_provider.h
    Signed-off-by: zjl_goldenglow <zhangjiale43@huawei.com>

--- utils/setting/include/setting_provider.h
+++ utils/setting/include/setting_provider.h
@@ -70,7 +70,6 @@ private:
     static constexpr const char* SETTING_POWER_MODE_BACKUP_KEY  {"settings.power.smart_mode_status.backup"};
     static constexpr const char* SETTING_POWER_WAKEUP_LID_KEY {"settings.power.wakeup_lid"};
     static constexpr const char* SETTING_DURING_CALL_STATE_KEY {"during_call_state"};
-
     static std::atomic<SettingProvider*> instance_;
     static sptr<IRemoteObject> remoteObj_;
     static int32_t currentUserId_;
