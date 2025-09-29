f37b9df9f246d8d7e4acaab55262ad220d8f94fe
zjl_goldenglow <zhangjiale43@huawei.com>
2025-09-29 21:42:38 +0800

    update: 更新文件 suspend_controller.h
    Signed-off-by: zjl_goldenglow <zhangjiale43@huawei.com>

--- services/native/src/suspend/suspend_controller.h
+++ services/native/src/suspend/suspend_controller.h
@@ -108,7 +108,7 @@ private:
     void ControlListener(SuspendDeviceType reason, uint32_t action, uint32_t delay);
 #ifdef POWER_MANAGER_ENABLE_CHARGING_TYPE_SETTING
     int64_t GetSettingPowerSleepTime(int64_t defaultTime);
-    uint32_t CalculateAutoSleepDelay();
+    int64_t CalculateAutoSleepResult();
 #endif
     void HandleAutoSleep(SuspendDeviceType reason);
     void SetAutoSleep(SuspendDeviceType reason);
