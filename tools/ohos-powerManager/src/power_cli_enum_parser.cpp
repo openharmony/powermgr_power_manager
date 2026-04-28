a1417a3ea028a3b776b4cc3fd16d2d3bcababbb7
hxf233333 <huxiaofei11@huawei.com>
2026-04-28 20:23:07 +0800

    feat: add power manager cli
    Signed-off-by: hxf233333 <huxiaofei11@huawei.com>
    
    Co-Authored-By: Agent

---
+++ tools/ohos-powerManager/src/power_cli_enum_parser.cpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "power_cli_enum_parser.h"
+
+namespace OHOS {
+namespace PowerMgr {
+
+static const std::unordered_map<std::string, PowerMode> POWER_MODE_MAP = {
+    {"normal",       PowerMode::NORMAL_MODE},
+    {"performance",  PowerMode::PERFORMANCE_MODE},
+};
+
+EnumParseResult PowerCliEnumParser::ParsePowerMode(const std::string& str)
+{
+    auto it = POWER_MODE_MAP.find(str);
+    if (it != POWER_MODE_MAP.end()) {
+        return {true, static_cast<int32_t>(it->second), ""};
+    }
+    return {false, 0, "Invalid power mode: '" + str + "'. Valid values: " + GetValidPowerModes()};
+}
+
+std::string PowerCliEnumParser::GetValidPowerModes()
+{
+    return "normal, performance";
+}
+
+} // namespace PowerMgr
+} // namespace OHOS
